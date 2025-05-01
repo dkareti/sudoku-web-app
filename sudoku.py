ac344b36754a6c17487ee00da7c12cd312d43868
Daniel Kareti <danielkareti@gmail.com>
2025-05-01 12:18:38 -0700

    Added descriptive comments to the code

--- sudoku.py
+++ sudoku.py
@@ -4,21 +4,33 @@
 import random
 import copy
 
-#checks for valid placement of a number at the specified row
-#and column of the grid
+# checks for valid placement of a number at the specified row
+# and column of the grid
 def is_valid(grid, row: int, col: int, num: int) -> bool:
-    #check the row and column to make sure 
-    #the rules of sudoku are followed
+    #   #   #   #   #   #   #   #   #   #
+    # check the row and column to make sure 
+    # the rules of sudoku are followed
+    #   #   #   #   #   #   #   #   #   #   
     if num in grid[row]:
         return False
     if num in [grid[i][col] for i in range(9)]:
         return False
     
-
-    #check the 3x3 grid
+    #   #   #   #   #   #   #   #   #   #   
+    # check the 3x3 sub-grid
+    # the following line finds the top-left corner of the 3x3
+    #   subgrid
+    #
+    # For Example,
+    # If row = 5, col = 7, then:
+	#   •	row // 3 = 1, so start_row = 3 * 1 = 3
+	#   •	col // 3 = 2, so start_col = 3 * 2 = 6
+    #
+    #   #   #   #   #   #   #   #   #   # 
     start_row, start_col = 3 * (row // 3), 3 * (col // 3)
     for i in range(start_row, start_row + 3):
         for j in range(start_col, start_col+3):
             if grid[i][j] == num:
                 return False
     return True
+
